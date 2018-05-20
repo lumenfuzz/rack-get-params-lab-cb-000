b4441c4dc5fef84f2ab3307914c3484eb717070a
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-20 20:24:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,13 @@ class Application
       else
         resp.write "Your cart is empty"
       end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
